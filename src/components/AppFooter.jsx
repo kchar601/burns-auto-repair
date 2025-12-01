7a2ec0de6bc6e2373c640c5597bc72894be1259e
Keith Charlton <charltonkeith8@gmail.com>
2025-11-30 19:11:48 -0500

    Revamp footer and navigation layout and styles
    Refactored AppFooter and AppNav components to improve structure and accessibility, including new grid layouts and additional sections in the footer. Updated CSS for better responsiveness, added new style variables, and enhanced social/contact info presentation. Changed call button to a link for improved usability.

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -4,68 +4,71 @@ import styles from "./AppNav.module.css";
 function AppNav({ theme }) {
   return (
     <footer className={styles.footbar}>
-      <NavLink className={styles.logoContainer} to="/">
-        {theme === "dark" ? (
-          <img
-            src="src\assets\burnsautologo white.png"
-            className={styles.logo}
-          ></img>
-        ) : (
-          <img
-            src="src\assets\burnsautologo dark.png"
-            className={styles.logo}
-          ></img>
-        )}
-        <img className={styles.logo} />
-      </NavLink>
-      <ul className={styles.links}>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? styles.active : "")}
-            to="/"
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? styles.active : "")}
-            to="/services"
-          >
-            Services
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? styles.active : "")}
-            to="/about"
-          >
-            About
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? styles.active : "")}
-            to="/testimonials"
-          >
-            Testimonials
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? styles.active : "")}
-            to="/contact"
-          >
-            Contact
-          </NavLink>
-        </li>
-      </ul>
-      <div className={styles.logoContainer}>
-        <button className={`cta ` + styles.callBtn}>
-          <i class="fa-solid fa-phone"></i>
-          215-968-3791
-        </button>
+      <div className={styles.footbarInner}>
+        <NavLink
+          className={`${styles.logoContainer} ${styles.logoContainerFooter}`}
+          to="/"
+        >
+          {theme === "dark" ? (
+            <img
+              src="src\assets\burnsautologo white.png"
+              className={styles.logo}
+            ></img>
+          ) : (
+            <img
+              src="src\assets\burnsautologo dark.png"
+              className={styles.logo}
+            ></img>
+          )}
+          <img className={styles.logo} />
+        </NavLink>
+        <ul className={`${styles.links} ${styles.linksVert}`}>
+          <h5>Quick Links</h5>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/services">Services</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/testimonials">Testimonials</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+        <ul className={`${styles.links} ${styles.linksVert}`}>
+          <h5>Services</h5>
+          <li>General Maintenance</li>
+          <li>PA Inspections</li>
+          <li>Oil Changes</li>
+          <li>Scheduled Maintenance</li>
+          <li>Brakes & Tires</li>
+          <li>AC & Heating</li>
+        </ul>
+        <ul className={`${styles.links} ${styles.linksVert} ${styles.socials}`}>
+          <h5>Contact Us</h5>
+          <li>(215) 968-3791</li>
+          <li>burnsauto19@gmail.com</li>
+          <li>19 N Sycamore Street</li>
+          <li className={styles.noMargin}>Newtown, PA 18940</li>
+          <div className={styles.socialsInner}>
+            <a>
+              <i className="fa-brands fa-facebook-f"></i>
+            </a>
+            <a>
+              <i class="fa-brands fa-instagram"></i>
+            </a>
+          </div>
+        </ul>
       </div>
+      <small className={styles.copywrite}>
+        © {new Date().getFullYear()} Burns Auto Repair, Inc. All rights
+        reserved.
+      </small>
     </footer>
   );
 }
--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -4,67 +4,69 @@ import styles from "./AppNav.module.css";
 function AppNav({ theme }) {
   return (
     <nav className={styles.navbar}>
-      <NavLink className={styles.logoContainer} to="/">
-        {theme === "dark" ? (
-          <img
-            src="src\assets\burnsautologo white.png"
-            className={styles.logo}
-          ></img>
-        ) : (
-          <img
-            src="src\assets\burnsautologo dark.png"
-            className={styles.logo}
-          ></img>
-        )}
-        <img className={styles.logo} />
-      </NavLink>
-      <ul className={styles.links}>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? styles.active : "")}
-            to="/"
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? styles.active : "")}
-            to="/services"
-          >
-            Services
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? styles.active : "")}
-            to="/about"
-          >
-            About
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? styles.active : "")}
-            to="/testimonials"
-          >
-            Testimonials
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? styles.active : "")}
-            to="/contact"
-          >
-            Contact
-          </NavLink>
-        </li>
-      </ul>
-      <div className={styles.logoContainer}>
-        <button className={`cta ` + styles.callBtn}>
-          <i class="fa-solid fa-phone"></i>
-          215-968-3791
-        </button>
+      <div className={styles.navbarInner}>
+        <NavLink className={styles.logoContainer} to="/">
+          {theme === "dark" ? (
+            <img
+              src="src\assets\burnsautologo white.png"
+              className={styles.logo}
+            ></img>
+          ) : (
+            <img
+              src="src\assets\burnsautologo dark.png"
+              className={styles.logo}
+            ></img>
+          )}
+          <img className={styles.logo} />
+        </NavLink>
+        <ul className={styles.links}>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? styles.active : "")}
+              to="/"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? styles.active : "")}
+              to="/services"
+            >
+              Services
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? styles.active : "")}
+              to="/about"
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? styles.active : "")}
+              to="/testimonials"
+            >
+              Testimonials
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={({ isActive }) => (isActive ? styles.active : "")}
+              to="/contact"
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+        <div className={styles.logoContainer}>
+          <a className={`cta ` + styles.callBtn} href="Tel: (215) 968-3791">
+            <i className="fa-solid fa-phone"></i>
+            215-968-3791
+          </a>
+        </div>
       </div>
     </nav>
   );
