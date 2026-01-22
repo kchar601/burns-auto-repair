1aac141a0183fa4fbbf3ec7f925a5773cf7f7d34
Keith Charlton <charltonkeith8@gmail.com>
2026-01-22 15:55:03 -0500

    Updated reviewList & CtaLink

--- src/components/CtaLink/CtaLink.jsx
+++ src/components/CtaLink/CtaLink.jsx
@@ -1,14 +1,24 @@
 import styles from "./CtaLink.module.css";
 import { NavLink } from "react-router-dom";
 
-function CtaLink({ link, icon, altColor, children }) {
+function CtaLink({ link, icon, altColor, iconEnd, targetBlank, children }) {
+  const classNames = `${styles.cta} ${icon ? styles.icon : ""} ${altColor ? styles.altColor : ""} ${iconEnd ? styles.iconEnd : ""}`;
+
+  if (targetBlank) {
+    return (
+      <a className={classNames} href={link} target="_blank">
+        {icon && !iconEnd ? <span>{icon}</span> : ""}
+        {children}
+        {icon && iconEnd ? <span>{icon}</span> : ""}
+      </a>
+    );
+  }
+
   return (
-    <NavLink
-      className={`${styles.cta} ${icon ? styles.icon : ""} ${altColor ? styles.altColor : ""}`}
-      to={link}
-    >
-      {icon ? <span>{icon}</span> : ""}
+    <NavLink className={classNames} to={link}>
+      {icon && !iconEnd ? <span>{icon}</span> : ""}
       {children}
+      {icon && iconEnd ? <span>{icon}</span> : ""}
     </NavLink>
   );
 }
--- src/components/ReviewList/ReviewList.jsx
+++ src/components/ReviewList/ReviewList.jsx
@@ -1,28 +1,49 @@
 import testimonials from "../../data/testimonials.json";
 import styles from "./ReviewList.module.css";
 import renderStars from "./../TestimonialsCarousel/RenderStars";
+import CtaLink from "./../CtaLink/CtaLink";
 
 function ReviewList() {
   return (
-    <section className={styles.container}>
-      {sortedTestimonials.map((t, i) => (
-        <div key={i} className={styles.card}>
-          <div className={styles.flexApart}>
-            <span className={styles.custName}>{t.name}</span>
-            <span>{t.date}</span>
-          </div>
-          <div>{renderStars(t.rating)}</div>
-          <p className={styles.cardHeader}>{t.title}</p>
+    <>
+      <section className={styles.container}>
+        <div className={styles.reviewsContent}>
+          {sortedTestimonials.map((t, i) => (
+            <div key={i} className={styles.card}>
+              <div className={styles.flexApart}>
+                <div className={styles.nameLine}>
+                  <span className={styles.custName}>{t.name}</span>
+                  <span>{t.date}</span>
+                </div>
+                <a className={styles.reviewLink} href={t.link} target="_blank">
+                  <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                </a>
+              </div>
+              <div>{renderStars(t.rating)}</div>
+              <p className={styles.cardHeader}>{t.title}</p>
 
-          <p>{t.text}</p>
+              <p>{t.text}</p>
+            </div>
+          ))}
         </div>
-      ))}
-    </section>
+        <span className={styles.seeAll}>
+          <CtaLink
+            altColor
+            iconEnd
+            icon={<i className="fa-solid fa-chevron-down"></i>}
+            link={"https://www.surecritic.com/reviews/burns-auto-repair-inc"}
+            targetBlank
+          >
+            See All Reviews
+          </CtaLink>
+        </span>
+      </section>
+    </>
   );
 }
 
 const sortedTestimonials = [...testimonials].sort(
-  (a, b) => new Date(b.date) - new Date(a.date)
+  (a, b) => new Date(b.date) - new Date(a.date),
 );
 
 export default ReviewList;
